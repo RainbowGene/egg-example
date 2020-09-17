202c5b15240b731559b3fbb6f4aff26a01114fe7
Gene <1767386755@qq.com>
2020-09-17 19:48:59 +0800

    关闭csrf开启跨域

--- config/config.default.js
+++ config/config.default.js
@@ -15,12 +15,27 @@ module.exports = appInfo => {
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1600341648045_8031';
 
-  // add your middleware config here
+  // 在这里添加中间件
   config.middleware = [];
 
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
+  };
+
+  // 关闭 csrf，开启跨域
+  config.security = {
+    csrf: {
+      enable: false,
+    },
+    // 跨域白名单
+    domainWhiteList: []
+  };
+
+  // 允许跨域的方法
+  config.cors = {
+    origin: '*',
+    allowMethods: 'GET,PUT,POST,DELETE,PATCH'
   };
 
   return {
